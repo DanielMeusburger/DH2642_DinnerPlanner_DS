e8f3c049a41b1b1c20a54f9c5e8efedc3259f8c1
Sudarson <ssudarson@gmail.com>
2016-02-16 00:56:25 +0100

    Skeleton of down view is added for third page

---
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/selectedcourse/SelectedCourseDownView.java
@@ -0,0 +1,14 @@
+package se.kth.csc.iprog.dinnerplanner.android.view.selectedcourse;
+
+import android.view.View;
+
+/**
+ * Created by sudarson on 09/02/16.
+ */
+public class SelectedCourseDownView {
+    View view;
+
+    public SelectedCourseDownView(View view) {
+        this.view = view;
+    }
+}
